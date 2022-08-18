8ffe80e866693c1b95304a156b84c033603f502d
Haris Rasidagic <haris.rasidagic1@gmail.com>
2022-08-18 03:20:43 +0200

    add: provisioning vpc and private ec2 instance

---
+++ production.tf
@@ -0,0 +1,26 @@
+resource "random_id" "random_id_prefix" {
+  byte_length = 2
+}
+
+# Using custom Networking module
+module "Networking" {
+  source               = "./modules/Networking"
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+}
+
+# Using ec2-instance module from terraform registry
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "ec2_private"
+
+  ami                    = "ami-065deacbcaac64cf2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.Networking.default_sg_id]
+  subnet_id              = flatten(module.Networking.private_subnets_id)[0]
+}
